619b474f785297cfd789df3e997c857bcd74b172
Seyit47 <seyitbu1111@gmail.com>
2023-06-30 01:04:42 +0500

    feat: add icons

--- src/composables/useScreen.ts
+++ src/composables/useScreen.ts
@@ -27,17 +27,27 @@ export const useScreen = () => {
     const current = ref<ScreenSize>(Size.SMALL);
 
     const getSize = (width?: number) => {
-        if (width === undefined) width = screenSize.width;
+        if (width === undefined) {
+            width = screenSize.width;
+        }
         const {
             [Size.SMALL]: sm,
             [Size.MEDIUM]: md,
             [Size.LARGE]: lg,
             [Size.EXTRA_LARGE]: xl,
         } = defaultScreenConfig;
-        if (width < Number(sm)) return Size.SMALL;
-        if (width < Number(md)) return Size.MEDIUM;
-        if (width < Number(lg)) return Size.LARGE;
-        if (width < Number(xl)) return Size.EXTRA_LARGE;
+        if (width < Number(sm)) {
+            return Size.SMALL;
+        }
+        if (width < Number(md)) {
+            return Size.MEDIUM;
+        }
+        if (width < Number(lg)) {
+            return Size.LARGE;
+        }
+        if (width < Number(xl)) {
+            return Size.EXTRA_LARGE;
+        }
         return Size.EXTRA_LARGE;
     };
 
@@ -56,21 +66,33 @@ export const useScreen = () => {
             [Size.EXTRA_LARGE]: xl,
         } = defaultScreenConfig;
         const width = screenSize.width;
-        if (size === Size.SMALL) return width >= Number(sm);
-        if (size === Size.MEDIUM) return width >= Number(md);
-        if (size === Size.LARGE) return width >= Number(lg);
-        if (size === Size.EXTRA_LARGE) return width >= Number(xl);
+        if (size === Size.SMALL) {
+            return width >= Number(sm);
+        }
+        if (size === Size.MEDIUM) {
+            return width >= Number(md);
+        }
+        if (size === Size.LARGE) {
+            return width >= Number(lg);
+        }
+        if (size === Size.EXTRA_LARGE) {
+            return width >= Number(xl);
+        }
         return false;
     };
 
     onMounted(() => {
-        if (typeof window === "undefined") return;
+        if (typeof window === "undefined") {
+            return;
+        }
         window.addEventListener("resize", onWindowResize);
         getSize(window.innerWidth);
     });
 
     onUnmounted(() => {
-        if (typeof window === "undefined") return;
+        if (typeof window === "undefined") {
+            return;
+        }
         window.removeEventListener("resize", onWindowResize);
     });
 
